e18ff9f19024f3ccc1b841b5e760fe2a1816b8fb
Andy Uppendahl <a.uppendahl@gmail.com>
2015-03-04 15:09:13 -0800

    Add functionality to add new departments via form.

--- controllers/new-department.js
+++ controllers/new-department.js
@@ -1,11 +1,19 @@
 Auction.NewDepartmentController = Ember.Controller.extend({
   actions: {
     save: function() {
-      var newDept = this.store.createRecord('department',
-        {title: this.get('depTitle'), imageURL: this.get('depImage')});
-
+      var newDept = this.store.createRecord('department', {
+        title: this.get('depTitle'),
+        imageURL: "../imgs/" + this.get('depImage')
+      });
       newDept.save();
       this.transitionToRoute('departments');
-    }
+    },
+
+    // delete: function() {
+    //   if (confirm("Must you? Reeeally?")) {
+    //     this.get('model').destroyRecord();
+    //     this.transitionToRoute('departments');
+    //   }
+    // }
   }
 });
