2250bfc615c89e8cb227b767d1f8a61748a34a91
Benjamin Herson <benjaminherson@gmail.com>
2015-03-05 16:59:15 -0800

    fix route bug

--- router.js
+++ router.js
@@ -2,11 +2,11 @@ Auction.Router.map(function() {
   this.resource('departments', {path: '/'}, function() {
     this.resource('new-department');
   });
-  this.resource('department', {path: '/:department_id'}, function() {
+  this.resource('department', {path: '/departments/:department_id'}, function() {
     this.resource('new-lot');
   });
-  this.resource('lot', {path: '/:lot_id'}, function() {
+  this.resource('lot', {path: '/lots/:lot_id'}, function() {
     this.resource('new-item');
   });
-  this.resource('item', {path: '/:item_id'});
+  this.resource('item', {path: '/items/:item_id'});
 });
